74c60851313b66c97e22f994ea17e5a710ca8a11
Daniela <danielaoancea2009@yahoo.ca>
2020-06-04 00:56:07 +0000

    add code into the file

---
+++ fetcher.js
@@ -0,0 +1,8 @@
+//command line node app which take a URL as a command-line argument and download the resource to the specified path.
+
+const request = require('request');
+request('http://www.google.com', (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
